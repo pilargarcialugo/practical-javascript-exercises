425e77afe2e89877c93da072e094ea66b8645f1d
pilargarcialugo <pilargarcialugo@gmail.com>
2021-08-09 22:52:49 -0500

    Call functions from HTML for calculate square measures.

--- figures/figures.js
+++ figures/figures.js
@@ -1,9 +1,15 @@
-function calculateSquarePerimeter(size) {
-    return size * 4;
+function calculateSquarePerimeter() {
+    const sizeInput = document.getElementById("squareSize");
+    const perimeter = sizeInput.value * 4;
+    alert("Perimeter = " + perimeter) 
+    return perimeter;
 }
 
-function calculateSquareArea(size) {
-    return size * size;
+function calculateSquareArea() {
+    const sizeInput = document.getElementById("squareSize");
+    const area = sizeInput.value * sizeInput.value;
+    alert("Area = " + area); 
+    return area;
 }
 
 function calculateTrianglePerimeter(base, side1, side2) {
@@ -25,3 +31,5 @@ function calculateCirclePerimeter(radio) {
 function calculateCircleArea(radio) {
     return (radio * radio) * Math.PI;
 }
+
+
