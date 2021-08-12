b40e6965595c877fd7d3a29942adb0bab39b3b52
pilargarcialugo <pilargarcialugo@gmail.com>
2021-08-11 21:59:22 -0500

    Isosceles Triangle Height calculation added with parameters from HTML

--- figures/figures.js
+++ figures/figures.js
@@ -32,4 +32,18 @@ function calculateCircleArea(radio) {
     return (radio * radio) * Math.PI;
 }
 
+function checkIsoscelesTriangleHeight() {
+    const sidea = document.getElementById("sidea").value;
+    const sideb = document.getElementById("sideb").value;
+    if (sidea === sideb) {
+        const height = calculateIsoscelesTriangleHeight(
+            document.getElementById("base").value, sidea);
+        alert("Heigth = " + height);
+    } else {
+        alert("The values entered do not correspond to an isosceles triangle");
+    }
+}
 
+function calculateIsoscelesTriangleHeight(base, sidea) {
+    return Math.sqrt(Math.pow(sidea, 2) - (Math.pow(base, 2) / 4));
+}
